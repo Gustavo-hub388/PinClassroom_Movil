19a87a62023ef870781836d47fd6ef009516e113
Gustavo_Flamenco <flemish.vladd3030@gmail.com>
2019-10-27 15:09:56 -0600

    Realizando la primera prueba de funcionalidad de las Tabs(Pestañas.

--- app/src/main/java/com/example/pinclassroom/AulasActivity.java
+++ app/src/main/java/com/example/pinclassroom/AulasActivity.java
@@ -3,12 +3,32 @@ package com.example.pinclassroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class AulasActivity extends AppCompatActivity {
+
+    TabHost contenedor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aulas);
+
+        contenedor = findViewById(R.id.contenedor_tabs);
+        contenedor.setup();
+
+        TabHost.TabSpec spec1 = contenedor.newTabSpec("Aulas");
+        spec1.setContent(R.id.tab_aula);
+        spec1.setIndicator("Aula");
+        contenedor.addTab(spec1);
+
+        TabHost.TabSpec spec2 = contenedor.newTabSpec("Mapas");
+        spec2.setContent(R.id.tab_mapa);
+        spec2.setIndicator("Mapa");
+        contenedor.addTab(spec2);
+
+        contenedor.setCurrentTab(0);
+
     }
 }
