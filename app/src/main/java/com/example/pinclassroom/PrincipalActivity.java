2a94151cf1bea653c00ca6a58cd977db92ec0890
Gustavo_Flamenco <flemish.vladd3030@gmail.com>
2019-10-27 13:59:54 -0600

    Implementando un DashBoard como menu principal del proyecto PinClassroom

--- app/src/main/java/com/example/pinclassroom/PrincipalActivity.java
+++ app/src/main/java/com/example/pinclassroom/PrincipalActivity.java
@@ -1,14 +1,43 @@
 package com.example.pinclassroom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class PrincipalActivity extends AppCompatActivity {
+public class PrincipalActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private CardView cardAulas, cardDocentes, cardEventos, cardConfiguraciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        //definiendo las tarjetas
+        cardAulas = findViewById(R.id.card_view_aulas);
+        cardDocentes = findViewById(R.id.card_view_docentes);
+        cardEventos = findViewById(R.id.card_view_eventos);
+        cardConfiguraciones = findViewById(R.id.card_view_config);
+
+        //agregando el evento click a las tarjetas
+        cardAulas.setOnClickListener(this);
+        cardDocentes.setOnClickListener(this);
+        cardEventos.setOnClickListener(this);
+        cardConfiguraciones.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()){
+            case R.id.card_view_aulas : i = new Intent(this, AulasActivity.class); startActivity(i); break;
+            case R.id.card_view_docentes : i = new Intent(this, DocentesActivity.class); startActivity(i); break;
+            case R.id.card_view_eventos : i = new Intent(this, EventosActivity.class); startActivity(i); break;
+            case R.id.card_view_config : i = new Intent(this, ConfigActivity.class); startActivity(i); break;
+            default:break;
+        }
     }
 }
